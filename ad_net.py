2f991be25a939a3b5076dd34bfdb8439900afc22
Sylvia Z <sylviazh3@gmail.com>
2022-01-27 22:19:34 +1100

    patch&stitchDC v2 changes of convnet config + padding*

--- ad_net.py
+++ ad_net.py
@@ -37,8 +37,8 @@ class AdversarialNetwork(nn.Module):
             # using a conv layer instead of a linear one -> performance gains
             # nn.Conv2d(in_channels, emb_size, kernel_size=patch_size, stride=patch_size),
             # Rearrange('b e (h) (w) -> b (h w) e'),
-            nn.Conv2d(in_feature, 64, kernel_size=4, stride=2), nn.LeakyReLU(0.2, True), # 15, 15
-            nn.Conv2d(64, 128, kernel_size=4, stride=2), norm_layer(128, affine=True, eps=eps), nn.LeakyReLU(0.2, True),  #6 * 6
+            nn.Conv2d(in_feature, 64, kernel_size=4, stride=2, padding=1), nn.LeakyReLU(0.2, True), # 15, 15
+            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1), norm_layer(128, affine=True, eps=eps), nn.LeakyReLU(0.2, True),  #6 * 6
             # nn.Conv2d(128, 256, kernel_size=4, stride=2), norm_layer(256, affine=True, eps=eps), ## output 2*2
             # Rearrange('b c (h) (w) -> b c (h w)'),
         )
@@ -57,7 +57,7 @@ class Domain_classifier(nn.Module):
         if ad_mode == 'universal':
             self.ad_net = AdversarialNetwork(1)
         elif ad_mode == 'local':
-            self.ad_net = nn.ModuleList([AdversarialNetwork(emb_size) for i in range(64)])
+            self.ad_net = nn.ModuleList([AdversarialNetwork(1) for i in range(64)])
         else:
             raise ValueError("ad_mode undifined")
         self.ad_mode = ad_mode
@@ -68,9 +68,9 @@ class Domain_classifier(nn.Module):
         #                             nn.Linear(128, 1),
         #                             Rearrange('b (h w) c -> b c h w', h=8))  # b, 64, 1 
         # self.classificationhead = nn.Conv2d(256, 1, 1)
-        self.classificationhead = nn.Sequential(nn.Conv2d(128, 256, kernel_size=4, stride=2), norm_layer(256, affine=True, eps=eps), nn.LeakyReLU(0.2, True), 
-                                                nn.Conv2d(256, 512, kernel_size=4, stride=2), norm_layer(512, affine=True, eps=eps), nn.LeakyReLU(0.2, True),
-                                                nn.Conv2d(512, 1, kernel_size=3, stride=1))
+        self.classificationhead = nn.Sequential(nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1), norm_layer(256, affine=True, eps=eps), nn.LeakyReLU(0.2, True), 
+                                                nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1), norm_layer(512, affine=True, eps=eps), nn.LeakyReLU(0.2, True),
+                                                nn.Conv2d(512, 1, kernel_size=4, stride=2, padding=1))
     def forward(self, x):
 
         ## crop out patches
@@ -88,7 +88,7 @@ class Domain_classifier(nn.Module):
             else:
                 pred += [self.ad_net[patch_id](patch)]
         
-        output = torch.cat(pred, -1).view(b,128,48,48)
+        output = torch.cat(pred, -1).view(b,128,64,64)
         # output = torch.stack(pred, -1).view(b,256,8,8)
         # print(output.size())
         # print(pred[0].size())
