6d83173208ba1bdaf98aca3b4594905a19ffd116
Sylvia Z <sylviazh3@gmail.com>
2022-01-27 15:05:01 +1100

    DC-patch&stitch

--- ad_net.py
+++ ad_net.py
@@ -39,13 +39,13 @@ class AdversarialNetwork(nn.Module):
             # Rearrange('b e (h) (w) -> b (h w) e'),
             nn.Conv2d(in_feature, 64, kernel_size=4, stride=2), nn.LeakyReLU(0.2, True), # 15, 15
             nn.Conv2d(64, 128, kernel_size=4, stride=2), norm_layer(128, affine=True, eps=eps), nn.LeakyReLU(0.2, True),  #6 * 6
-            nn.Conv2d(128, 256, kernel_size=4, stride=2), norm_layer(256, affine=True, eps=eps), ## output 2*2
-            Rearrange('b c (h) (w) -> b c (h w)'),
+            # nn.Conv2d(128, 256, kernel_size=4, stride=2), norm_layer(256, affine=True, eps=eps), ## output 2*2
+            # Rearrange('b c (h) (w) -> b c (h w)'),
         )
 
     def forward(self, x):
         # print(x.size())
-        x = self.sequence(x).mean(-1)
+        x = self.sequence(x)#.mean(-1)
         # x = self.sigmoid(x)
         return x
 
@@ -53,6 +53,7 @@ class Domain_classifier(nn.Module):
     def __init__(self, ad_mode='universal'):
         super(Domain_classifier, self).__init__()
         eps = 0.001
+        norm_layer=nn.InstanceNorm2d
         if ad_mode == 'universal':
             self.ad_net = AdversarialNetwork(1)
         elif ad_mode == 'local':
@@ -62,11 +63,14 @@ class Domain_classifier(nn.Module):
         self.ad_mode = ad_mode
         # self.resnetblock = nn.Sequential(nn.Conv2d(128, 128, 3, padding = 1), nn.InstanceNorm2d(128, affine=True, eps=eps), nn.LeakyReLU(0.2, True),
         #                                         nn.Conv2d(128, 128, 3, padding = 1), nn.InstanceNorm2d(128, affine=True, eps=eps),)
-        self.linear = nn.Sequential(Rearrange('b c l -> b l c'),   # b, 256, 64 -> b, 64, 256
-                                    nn.Linear(256, 128), nn.LeakyReLU(), nn.Dropout(0.5),
-                                    nn.Linear(128, 1),
-                                    Rearrange('b (h w) c -> b c h w', h=8))  # b, 64, 1 
+        # self.linear = nn.Sequential(Rearrange('b c l -> b l c'),   # b, 256, 64 -> b, 64, 256
+        #                             nn.Linear(256, 128), nn.LeakyReLU(), nn.Dropout(0.5),
+        #                             nn.Linear(128, 1),
+        #                             Rearrange('b (h w) c -> b c h w', h=8))  # b, 64, 1 
         # self.classificationhead = nn.Conv2d(256, 1, 1)
+        self.classificationhead = nn.Sequential(nn.Conv2d(128, 256, kernel_size=4, stride=2), norm_layer(256, affine=True, eps=eps), nn.LeakyReLU(0.2, True), 
+                                                nn.Conv2d(256, 512, kernel_size=4, stride=2), norm_layer(512, affine=True, eps=eps), nn.LeakyReLU(0.2, True),
+                                                nn.Conv2d(512, 1, kernel_size=3, stride=1))
     def forward(self, x):
 
         ## crop out patches
@@ -84,11 +88,17 @@ class Domain_classifier(nn.Module):
             else:
                 pred += [self.ad_net[patch_id](patch)]
         
-        output = torch.stack(pred, -1)#.view(b,128,8,8)
+        output = torch.cat(pred, -1).view(b,128,48,48)
+        # output = torch.stack(pred, -1).view(b,256,8,8)
         # print(output.size())
         # print(pred[0].size())
         # output = output + self.resnetblock(output)
-        output = self.linear(output)
+        # output = self.linear(output)
         # output = torch.stack(pred).view(b,128,8,8)
+
+        # output = self.ad_net(x)
+
+        output = self.classificationhead(output)
+        # output = torch.nn.functional.interpolate(input, size=None, scale_factor=None, mode='nearest')
 
         return output
